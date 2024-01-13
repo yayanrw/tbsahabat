78664931e71417b65efef1eb9ff939cb5e89f32d
yayanrw <yayanraw@gmail.com>
2024-01-14 06:39:53 +0700

    feat: create Banner.php, BannerModel.php, BannerView.php and BannerJs.php

---
+++ application/modules/Banner/models/BannerModel.php
@@ -0,0 +1,73 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+require_once APPPATH . 'core/Helper.php';
+
+class BrandModel extends CI_Model
+{
+	private $tableName = 'm_banner';
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function GetAll()
+	{
+		$data = $this->db->get_where($this->tableName, ['is_active' => 1])->result();
+		return $data;
+	}
+
+	public function Get($id)
+	{
+		$data = $this->db->get_where($this->tableName, ['id' => $id])->row();
+		return $data;
+	}
+
+	public function Insert($data)
+	{
+		$helper = new Helper();
+
+		$insert = array(
+			'title' => $helper->NullSafety($data['title']),
+			'sub_title' => $helper->NullSafety($data['sub_title']),
+			'description' => $helper->NullSafety($data['description']),
+			'img_url' => $helper->NullSafety($data['img_url']),
+			'is_active' => $helper->NullSafety($data['is_active']),
+			'created_by' => $this->session->userdata('user_id'),
+		);
+		$this->db->insert($this->tableName, $insert);
+	}
+
+	public function Update($data)
+	{
+		$helper = new Helper();
+		$update = array(
+			'title' => $helper->NullSafety($data['title']),
+			'sub_title' => $helper->NullSafety($data['sub_title']),
+			'description' => $helper->NullSafety($data['description']),
+			'img_url' => $helper->NullSafety($data['img_url']),
+			'is_active' => $helper->NullSafety($data['is_active']),
+			'updated_by' => $this->session->userdata('user_id'),
+		);
+		$this->db->where('id', $data['id']);
+		$this->db->update($this->tableName, $update);
+	}
+
+	public function UpdateActive($id, $is_active)
+	{
+		$update = array(
+			'is_active' => $is_active,
+			'updated_by' => $this->session->userdata('user_id'),
+		);
+		$this->db->where('id', $id);
+		$this->db->update($this->tableName, $update);
+	}
+
+	public function Delete($id)
+	{
+		return $this->db->delete($this->tableName, ['id' => $id]);
+	}
+}
+
+/* End of file filename.php */
---
+++ application/modules/Banner/views/BannerView.php
@@ -0,0 +1,44 @@
+<div class="container">
+	<div class="row">
+		<div class="col">
+			<div class="page-description">
+				<h1><?= $titleName; ?></h1>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col">
+			<div class="card">
+				<div class="card-header">
+					<h5 class="card-title"><?= $titleName; ?> Data </h5>
+				</div>
+				<div class="card-body">
+					<table id="table-data" class="table table-hover non-hover" style="width:100%">
+						<thead>
+							<tr>
+								<th>No</th>
+								<th>Title</th>
+								<th>SubTitle</th>
+								<th>Description</th>
+								<th>Status</th>
+								<th>Action</th>
+							</tr>
+						</thead>
+						<tbody>
+						</tbody>
+						<tfoot>
+							<tr>
+								<th>No</th>
+								<th>Title</th>
+								<th>SubTitle</th>
+								<th>Description</th>
+								<th>Status</th>
+								<th>Action</th>
+							</tr>
+						</tfoot>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
